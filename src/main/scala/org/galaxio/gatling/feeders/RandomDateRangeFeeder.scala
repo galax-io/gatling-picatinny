b1d2903ea8ca82f09b502bdde56c071805d221c2
John Axe <jigarkhwar88@gmail.com>
2024-12-11 05:00:43 -0300

    Add support for negative date ranges in feeders
    Refactored `RandomDateRangeFeeder` to handle negative offset dates, added `negativeDateRange` feeder example, and updated related utilities and tests. This improves flexibility in generating dynamic date ranges.

--- src/main/scala/org/galaxio/gatling/feeders/RandomDateRangeFeeder.scala
+++ src/main/scala/org/galaxio/gatling/feeders/RandomDateRangeFeeder.scala
@@ -6,18 +6,25 @@ import java.time.temporal.{ChronoUnit, TemporalUnit}
 import io.gatling.core.feeder.Feeder
 import org.galaxio.gatling.utils.RandomDataGenerators
 
+
 object RandomDateRangeFeeder {
 
   def apply(
-      paramNameFrom: String,
-      paramNameTo: String,
-      offsetDate: Long,
-      datePattern: String = "yyyy-MM-dd",
-      dateFrom: LocalDateTime = LocalDateTime.now(),
-      unit: TemporalUnit = ChronoUnit.DAYS,
-      timezone: ZoneId = ZoneId.systemDefault(),
-  ): Feeder[String] =
-    feeder[String](paramNameFrom)(dateFrom.format(DateTimeFormatter.ofPattern(datePattern)))
-      .map(m => m + (paramNameTo -> RandomDataGenerators.randomDate(offsetDate, datePattern, dateFrom, unit, timezone)))
+             paramNameFrom: String,
+             paramNameTo: String,
+             offsetDate: Long,
+             datePattern: String = "yyyy-MM-dd",
+             dateFrom: LocalDateTime = LocalDateTime.now(),
+             unit: TemporalUnit = ChronoUnit.DAYS,
+             timezone: ZoneId = ZoneId.systemDefault(),
+           ): Feeder[String] = {
+    val dateFormatter = DateTimeFormatter.ofPattern(datePattern)
+
+    def addRandomDateToMap(originalMap: Map[String, String]): Map[String, String] =
+      originalMap + (paramNameTo -> RandomDataGenerators.randomDate(offsetDate, datePattern, dateFrom, unit, timezone))
+
+    feeder[String](paramNameFrom)(dateFrom.format(dateFormatter)).map(addRandomDateToMap)
+  }
+
 
 }
