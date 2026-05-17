8b000806a641c23f0dd922b669ddf0f669484d7c
John <jigarkhwar88@gmail.com>
2026-05-17 09:45:53 -0300

    chore: bump Gatling to 3.13.5 on latest/gatling branch

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object Dependencies {
-  private val GatlingVersion          = "3.11.5"
+  private val GatlingVersion          = "3.13.5"
   lazy val gatlingCore: Seq[ModuleID] = Seq(
     "io.gatling" % "gatling-core",
     "io.gatling" % "gatling-core-java",
